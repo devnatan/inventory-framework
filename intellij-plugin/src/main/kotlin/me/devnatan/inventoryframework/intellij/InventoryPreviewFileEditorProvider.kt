a4e239180b8653633c7cb9c30d2097c34e7af7ac
Natan <natanvnascimento@gmail.com>
2026-07-30 17:39:18 -0300

    fix: mark provider DumbAware so HIDE_DEFAULT_EDITOR is allowed
    HIDE_DEFAULT_EDITOR requires the platform to trust the provider's
    accept() during dumb mode, which it now can since ViewDetector already
    short-circuits to false while indexing instead of throwing.

--- intellij-plugin/src/main/kotlin/me/devnatan/inventoryframework/intellij/InventoryPreviewFileEditorProvider.kt
+++ intellij-plugin/src/main/kotlin/me/devnatan/inventoryframework/intellij/InventoryPreviewFileEditorProvider.kt
@@ -6,10 +6,11 @@ import com.intellij.openapi.fileEditor.FileEditorProvider
 import com.intellij.openapi.fileEditor.TextEditor
 import com.intellij.openapi.fileEditor.TextEditorWithPreview
 import com.intellij.openapi.fileEditor.impl.text.TextEditorProvider
+import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 
-class InventoryPreviewFileEditorProvider : FileEditorProvider {
+class InventoryPreviewFileEditorProvider : FileEditorProvider, DumbAware {
 
     override fun accept(project: Project, file: VirtualFile): Boolean = ViewDetector.isViewFile(project, file)
 
